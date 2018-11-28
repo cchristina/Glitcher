fa13119940203146b6657fc4c920bc9e2c7315e2
Christina <christina.cuneo@gmail.com>
2018-11-28 20:22:39 +0000

    got dropzone upload working with dedicated page

--- server.py
+++ server.py
@@ -2,26 +2,45 @@
 
 from jinja2 import StrictUndefined
 
-from flask import (Flask, render_template, redirect, request, flash, session)
+from flask import (Flask, render_template, redirect, request, flash, session, url_for)
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_dropzone import Dropzone
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+import os
 
 from datetime import datetime, timedelta 
 
 from model import ImageChoice, Glitch, connect_to_db, db
+
+
+
 
 # from glitchstuff import *
 
 from datetime import * 
 
 
+
+
 import glitchstuff as gs
 
 from PIL import Image
 
+
+
+
+
+
+
+
 app = Flask(__name__)
 
+
+
 # Required to use Flask sessions and the debug toolbar
-app.secret_key = "a1b2c3"
+# app.secret_key = "a1b2c3"
+app.config['SECRET_KEY'] = 'supersecretkeygoeshere'
+
 
 # Normally, if you use an undefined variable in Jinja2, it fails
 # silently. This is horrible. Fix this so that, instead, it raises an
@@ -29,10 +48,155 @@ app.secret_key = "a1b2c3"
 app.jinja_env.undefined = StrictUndefined
 
 
+dropzone = Dropzone(app)
+# Dropzone settings
+#https://medium.com/@dustindavignon/upload-multiple-images-with-python-flask-and-flask-dropzone-d5b821829b1d
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+
+
+
+# Uploads settings
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/../static/images/uploads'
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  # set maximum file size, default is 16MB
+
+
+
+# @app.route('/')
+# def index():
+#     """Homepage."""
+#     return render_template("home.html")
+
+
+
+###START WORKING COODE ###
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+    
+#     # set session for image results
+#     if "file_urls" not in session:
+#         session['file_urls'] = []
+#     # list to hold our uploaded image urls
+#     file_urls = session['file_urls']
+#     # handle image upload from Dropzone
+#     if request.method == 'POST':
+#         file_obj = request.files
+#         for f in file_obj:
+#             file = request.files.get(f)
+            
+#             # save the file with to our photos folder
+#             filename = photos.save(
+#                 file,
+#                 name=file.filename    
+#             )
+#             # append image urls
+#             file_urls.append(photos.url(filename))
+            
+#         session['file_urls'] = file_urls
+#         return "uploading..."
+#     # return dropzone template on GET request    
+#     return render_template('index.html')
+# @app.route('/results')
+# def results():
+    
+#     # redirect to home if no images to display
+#     if "file_urls" not in session or session['file_urls'] == []:
+#         return redirect(url_for('index'))
+        
+#     # set the file_urls and remove the session variable
+#     file_urls = session['file_urls']
+#     session.pop('file_urls', None)
+
+#     for fu in file_urls:
+#         #../static/images/
+#         splt_fu = fu.split('/')
+#         new_url = "/static/images/uploads/" + splt_fu[-1]
+#         new_image = ImageChoice(url=new_url)
+
+#         db.session.add(new_image)
+#         print(new_image)
+#         db.session.commit()
+
+    
+#     return render_template('results.html', file_urls=file_urls)
+### END WORKING CODE ###
+
+
+
+### START NOT WORKING ###
 @app.route('/')
 def index():
     """Homepage."""
     return render_template("home.html")
+
+
+
+
+@app.route('/uploadImage', methods=['GET', 'POST'])
+def upload_image():
+    
+    # set session for image results
+    if "file_urls" not in session:
+        # print("file urls not in session**********")
+        session['file_urls'] = []
+
+
+    # list to hold our uploaded image urls
+    file_urls = session['file_urls']
+    # handle image upload from Dropzone
+    if request.method == 'POST':
+        print("IM GETTING THIS FAR LOOK AT ME LOOK AT ME")
+        file_obj = request.files
+        for f in file_obj:
+            file = request.files.get(f)
+            
+            # save the file with to our photos folder
+            filename = photos.save(
+                file,
+                name=file.filename    
+            )
+            # append image urls
+            file_urls.append(photos.url(filename))
+            print(filename, "****************************")
+            
+        session['file_urls'] = file_urls
+        return "uploading..."
+    # return dropzone template on GET request  
+    print("I guess it's a get rquest but wejjh#################")  
+    return render_template('uploadimage.html')
+
+
+
+@app.route('/results')
+def results():
+    
+    # redirect to home if no images to display
+    if "file_urls" not in session or session['file_urls'] == []:
+        return redirect(url_for('upload_image'))
+        
+    # set the file_urls and remove the session variable
+    file_urls = session['file_urls']
+    session.pop('file_urls', None)
+
+    for fu in file_urls:
+        #../static/images/
+        print(fu, "FUUUUUFUFUFUFUFUFUFUFUFU")
+        splt_fu = fu.split('/')
+        new_url = "../static/images/uploads/" + splt_fu[-1]
+        new_image = ImageChoice(url=new_url)
+
+        db.session.add(new_image)
+        print(new_image)
+        db.session.commit()
+
+    
+    return render_template('results.html', file_urls=file_urls)
+
+###END NOT WORKINH ###
 
 @app.route('/mainpage', methods = ["GET", "POST"])
 def mainpage():
@@ -97,16 +261,27 @@ def mainpage():
         canvas_height = image1.size[1]
 
 
-
-
-
-
-
         #str(datetime.now())+".jpg"
 
 
 
         return render_template("glitchpage.html", images = images, oldImage = (request.form.get("imagelist")[3:]), newImage=img3String, canvas_height = canvas_height, canvas_width = canvas_width)
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/imageUpload', methods=['GET', 'POST'])
+def upload_file():
+
+    print(request.form.get('dropzoneUpload'), request.files, "*************&&&&&&&&&&&&&&&&&&")
+    new_image = Image.open(request.form.get('dropzoneUpload'))
+    new_image.save('UPLOAD_FOLDER'+filename)
+
+
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
---
+++ static/scripts/CA.js
@@ -0,0 +1,358 @@
+"use strict";
+
+
+let matrixSize = 128;
+
+let cellSize = 8; 
+
+let cellMatrix = [];
+let neighborDict = {};
+
+
+let repDict = { 0:'_', 1:'█'}
+
+
+
+
+
+function populateBoard(){  
+
+
+    let matrix = [] 
+
+
+    for (let i = 0; i < matrixSize ; i++) {
+        let cellRow = [];
+
+        for (let j = 0; j < matrixSize ; j++){
+            let cellVal=0;
+            
+            if (Math.floor(Math.random() * 11) > 6){
+                cellVal = 1 ;
+            }
+            
+            cellRow.push(cellVal);
+        }    
+        matrix.push(cellRow);
+    }   
+
+    return matrix;
+
+}
+
+
+function populateOneD(){
+
+    let tempCells = []
+
+    for (let i = 0; i < matrixSize; i++){
+
+        tempCells.push(Math.random() * 2);
+
+
+    }
+
+
+    return tempCells;
+}
+
+
+
+function oneDRuleMaker(left, center, right, output){
+
+    // list_output = [int(i) for i in str(output)]
+
+        let listOutput = []
+        let sOut = output.toString();
+
+        for (let i = 0; i < sOut.length; i++){
+
+            listOutput.push(sOut[i]);
+        }
+
+
+    if (left && center && right){ //000
+        return listOutput[0]
+
+    }else if (left && center && !right ){ //001
+        return listOutput[1]
+
+    }else if (left && !center && right) { //010
+      return listOutput[2]
+
+    }else if (left && !center && !right ){ //011
+        return listOutput[3]
+ 
+    }else if (!left &&center && right){ //100
+        return listOutput[4]
+    
+    }else if (!left && center && !right){ //101
+        return listOutput[5]
+
+    } else if (!left && !center && right){ //110
+        return listOutput[6]
+
+    }else if (!left && !center && !right){
+        return listOutput[7]
+    }
+
+}
+
+
+
+
+
+function oneDNextRow(row, rules){
+
+    let newRow = []
+
+    for (i=0; i<row.length; i++){
+        if (i==0 || i==row.length-1){
+            newRow.push(row[i]);
+        }
+        else{
+
+            newRow.push(oneDNextRow(row[i-1], row[i]), row[i+1], rules);
+
+        }
+    }
+
+    return newRow;
+}
+
+
+
+function addRows(matrix, row, rules){
+
+    rules = rules.toString(2);
+    
+    matrix.push(row);
+    for (let i = 1; i < matrixSize; i++){
+        matrix.push(matrix[i-1], rules)
+    }
+
+
+}
+
+
+
+
+ 
+
+
+
+    
+
+
+function flipCell(x, y){
+
+
+
+    // console.log("PRE: " + cellMatrix[x][y])
+
+    if (cellMatrix[x][y]==0){
+    
+        cellMatrix[x][y]=1;
+    }    
+    else{
+
+        cellMatrix[x][y]=0;
+    }
+
+
+    // console.log("POST: " + cellMatrix[x][y])
+
+
+    
+}
+
+
+function getCell(x,y){
+
+    return cellMatrix[x][y];
+}
+
+
+
+function generateNeighborDictionary(neighborhood){
+
+
+    let neibDict = {}
+    let size = matrixSize
+
+    for (let i= 0; i < matrixSize; i++) {
+        for (let j = 0; j < matrixSize; j++){
+
+            let neighborList = []
+ 
+            for (let w=i-1; w < i+2 ; w++){
+                for (let q=j-1; q < j+2; q++){
+
+                    if (w!=i || q!=j ){
+
+                        try{
+
+                            if (neighborhood[w][q] != undefined){
+
+                                neighborList.push(neighborhood[w][q]);
+                                   
+                            }
+                        }catch(err){
+
+                            neighborList.push(Math.random() * 2);
+
+                        }
+                        
+                    }
+                }
+
+            }
+
+
+            let neighborKey = [i,j];  
+
+            neibDict[neighborKey] = neighborList;
+
+        // console.log(neibDict[neighborKey], neighborList)
+    }
+
+
+
+}
+
+// console.log(neibDict)
+return neibDict;
+
+}
+
+
+
+
+
+function gameOfLifeRound(neighborhood) {
+
+    let newNeighborhood = []
+
+    neighborDict = generateNeighborDictionary(neighborhood);
+    // console.log(neighborDict)
+
+    for (let i = 0; i < matrixSize ; i++) {
+
+        let newRow = [];
+
+    for (let j = 0; j < matrixSize; j++) {
+
+        // let neighborCount = neighborDict[[i,j]].reduce((acc, val) => 
+        // {
+        //     return acc + val;
+        // });
+
+        let neighborCount = 0;
+        let countKey = [i,j]
+
+        for (let i =0; i<neighborDict[countKey].length; i++) {
+
+            neighborCount+=neighborDict[countKey][i];
+        }
+
+        let cell = neighborhood[i][j];
+
+        if (cell ==1 ) {
+
+            if (neighborCount==2 || neighborCount == 3) {
+                newRow.push(1); 
+
+            }
+            else {
+                newRow.push(0);           
+            }
+        }
+        else if(cell==0 && neighborCount==3) {
+            newRow.push(1);
+        }
+        else{
+            newRow.push(0);
+        }
+    }  
+
+    newNeighborhood.push(newRow);
+
+    }
+
+    return newNeighborhood;    
+}
+
+
+function printRep(matrix){
+    for (let i = 0; i < matrix.length; i++){
+        for (let j=0; j< matrix.length; j++){
+
+          process.stdout.write(repDict[cellMatrix[i][j]]);
+        }
+
+        process.stdout.write('\n');
+        
+    }
+    console.log('\n\n');
+}
+
+
+
+
+
+
+
+    
+
+
+
+
+function displayRects(img){
+
+
+
+    // cellMatrix = gameOfLifeRound(cellMatrix);
+
+    for (let j = 0; j < cellMatrix.length-9; j++){
+
+        ctx.beginPath();
+        ctx.strokeStyle="#cccccc";
+        ctx.moveTo(j,0);
+        ctx.lineTo(j, cellMatrix.length);
+        ctx.stroke();
+
+        for (let k = 0; k<cellMatrix.length-9; k++){
+
+            if (cellMatrix[j+1][k+1]==1){
+
+                ctx.beginPath();
+                ctx.lineWidth=".3";
+                ctx.strokeStyle="#cccccc";
+                ctx.fillStyle = "#4010aa";
+                // ctx.globalAlpha = 0.9;
+                ctx.drawImage(img, j*cellSize, k*cellSize, cellSize, cellSize, j*cellSize-cellSize,k*cellSize, cellSize, cellSize);
+                ctx.restore();
+
+                // ctx.rect(j*cellSize,k*cellSize, cellSize, cellSize);
+                ctx.stroke();
+
+            }
+        }  
+    }
+}
+
+
+
+
+cellMatrix = populateBoard();
+
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+    
+
+
+
+
+ 
+
+
